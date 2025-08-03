40cf62f29895bb9890b34e707df38ec449bfc3d8
Daniel <mihaidaniel.it@gmail.com>
2025-08-03 20:23:20 +0300

    Added storage field for tasks

--- src/main/java/todo/manager/repository/Tasks.java
+++ src/main/java/todo/manager/repository/Tasks.java
@@ -1,4 +1,21 @@
 package todo.manager.repository;
 
+import todo.manager.services.model.Task;
+
+import java.util.ArrayList;
+
 public class Tasks {
+    /**
+     * @param tasks used for storing object of class Task
+     *              We can store object and use them in class TaskService
+     */
+    ArrayList<Task> tasks = new ArrayList<>();
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
 }
